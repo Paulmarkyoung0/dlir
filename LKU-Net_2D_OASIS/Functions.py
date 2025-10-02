7a009884f005706acca214c2eb260931207d1a5b
Paul Young <paulmarkyoung0@gmail.com>
2025-10-02 14:56:34 -0500

    zero padding

--- LKU-Net_2D_OASIS/Functions.py
+++ LKU-Net_2D_OASIS/Functions.py
@@ -9,12 +9,35 @@ import torch.utils.data as Data
 from pathlib import Path
 from skimage import io
 from torchvision.transforms.functional import to_tensor
+from torch.nn.functional import pad
 
 def load_images(image_dir, supervision):
+    """
+    Load and process histology and OCT images with optional segmentation masks.
+    
+    Args:
+        image_dir: Path to directory containing image files
+        supervision: Type of supervision ('dice', 'truth', or None)
+    
+    Returns:
+        Tuple of tensors depending on supervision type
+    """
     hist_path = image_dir / 'hist.png'
     hist_img = to_tensor(io.imread(hist_path))
     oct_path = image_dir / 'oct.png'
     oct_img = to_tensor(io.imread(oct_path, as_gray=True))
+    oct_dims = oct_img.shape[1:]
+    
+    hist_dims = hist_img.shape[1:]
+    pad_list = []
+    for i in range(1, -1, -1):
+        diff = oct_dims[i] - hist_dims[i]
+        pad_left = diff // 2
+        pad_right = diff - pad_left
+        pad_list.extend([pad_left, pad_right])
+    
+    hist_img = pad(hist_img, pad_list, mode='constant', value=0)
+
     if supervision == 'dice':
         hist_seg_path = image_dir / 'hist_seg.png'
         hist_seg = to_tensor(io.imread(hist_seg_path, as_gray=True))
